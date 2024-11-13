0b791d2d088f3ddfcfb13abc45d81315153df8db
DESKTOP-N5NA4SG\20117 <lks609591@gmail.com>
2024-11-13 19:07:13 +0900

    footer 작성 완료(링크 차후 추가)/sidebar 작성중

--- src/components/commons/footer/Footer.js
+++ src/components/commons/footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import '../../../css/components/Footer.css'
+import '../../../css/components/Footer.css';
+import banghyangLogo from '../../../images/banghyangLogo.png';
 
 const Footer = () => {
     // 네비게이션 링크 데이터
@@ -30,7 +31,6 @@ const Footer = () => {
 
     // 링크 클릭 핸들러
     const handleLinkClick = (e, path) => {
-        // 실제 라우팅 구현 전까지 preventDefault
         e.preventDefault();
         console.log(`Navigating to: ${path}`);
     };
@@ -38,46 +38,66 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-container">
-                <nav className="footer-nav">
-                    {navLinks.map((link, index) => (
-                        <a
-                            key={index}
-                            href={link.path}
-                            onClick={(e) => handleLinkClick(e, link.path)}
-                        >
-                            {link.title}
-                        </a>
-                    ))}
-                </nav>
-
-                <div className="footer-social">
-                    {socialLinks.map((link, index) => (
-                        <a
-                            key={index}
-                            href={link.path}
-                            onClick={(e) => handleLinkClick(e, link.path)}
-                        >
-                            {link.title}
-                        </a>
-                    ))}
+                <div className="footer-logo-section">
+                    <img 
+                        src={banghyangLogo}
+                        alt="방향 로고"
+                        className="footer-logo"
+                    />
                 </div>
 
-                <div className="footer-legal">
-                    {legalLinks.map((link, index) => (
-                        <a
-                            key={index}
-                            href={link.path}
-                            onClick={(e) => handleLinkClick(e, link.path)}
-                        >
-                            {link.title}
-                        </a>
-                    ))}
-                </div>
+                <div className="footer-textbox">
+                    <div className="footer-slogan">당신만의 특별한 향기</div>
+                    <nav className="footer-nav">
+                        {navLinks.map((link, index) => (
+                            <React.Fragment key={index}>
+                                <a
+                                    href={link.path}
+                                    onClick={(e) => handleLinkClick(e, link.path)}
+                                >
+                                    {link.title}
+                                </a>
+                                {index < navLinks.length - 1 && <span className="separator">|</span>}
+                            </React.Fragment>
+                        ))}
+                    </nav>
 
-                <div className="footer-info">
-                    회사명: 방향 | 서비스명: 방향(芳香) | 위치: 서울특별시 강남구 테헤란로 123 사당차 동북빌딩<br />
-                    통신판매업 신고번호: 제 2024-서울강남-1234호 대표자: 홍길동 | 고객센터: 02-1234-5678<br />
-                    이메일: support@banghyang.com
+                    <div className="footer-social">
+                        {socialLinks.map((link, index) => (
+                            <a
+                                key={index}
+                                href={link.path}
+                                onClick={(e) => handleLinkClick(e, link.path)}
+                            >
+                                {link.title}
+                            </a>
+                        ))}
+                    </div>
+
+                    <div className="footer-company-info">
+                        회사명: 올포원 | 서비스명: 방향(訪香) | 위치: 서울특별시 강남구 테헤란로 123 | 사업자 등록번호: 123-45-67890<br/>
+                        통신판매업 신고번호: 제 2024-서울강남-1234호 | 대표자: 홍길동 | 고객센터: 02-1234-5678 | 이메일: support@banghyang.com
+                    </div>
+
+                    <div className="footer-bottom-block">
+                        <div className="footer-legal">
+                            {legalLinks.map((link, index) => (
+                                <React.Fragment key={index}>
+                                    <a
+                                        href={link.path}
+                                        onClick={(e) => handleLinkClick(e, link.path)}
+                                    >
+                                        {link.title}
+                                    </a>
+                                    {index < legalLinks.length - 1 && <span className="separator">|</span>}
+                                </React.Fragment>
+                            ))}
+                        </div>
+
+                        <div className="footer-copyright">
+                            © 2024 방향. All rights reserved.
+                        </div>
+                    </div>
                 </div>
             </div>
         </footer>
