3c36a295ca029885432b89b779f1f8483db4b0bb
DESKTOP-G0504UF\20116 <wdh970616@naver.com>
2024-11-19 09:46:31 +0900

    로그인 테스트 페이지

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import React from 'react';
+import LoginPage from "./pages/LoginPage";
+import KakaoRedirectPage from "./pages/KakaoRedirectPage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Main/>}>
-          <Route index element={<Main/>}/>
+          <Route path="/" element={<Main />}>
+            <Route index element={<Main />} />
 
           </Route>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/oauth/redirected/kakao" element={<KakaoRedirectPage />} />
         </Routes>
       </BrowserRouter>
     </>
