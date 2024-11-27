56649b325bcd2a7ba8fb817a752d09f6051bee41
DESKTOP-6C9MP2O\20113 <jklun080921@gmail.com>
2024-11-27 16:09:47 +0900

    향료 DB 연동 향료 DB 연동, 필터링 수정, 카드 뒤집기 및 체크박스 수정, 로딩화면 구현

--- src/Store.js
+++ src/Store.js
@@ -2,11 +2,13 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import authReducer from "./module/AuthModule";
 import memberReducer from "./module/MemberModule";
+import spiceReducer from "./module/SpicesModule";
 
 // 여러 리듀서를 합치는 경우
 const rootReducer = combineReducers({
     auth: authReducer,
-    members: memberReducer
+    members: memberReducer,
+    spices: spiceReducer
 });
 
 // 스토어 생성
