49c87e326800d8972990c72e91cb19a7ea1cbf6a
박성은 <jklun080921@gmail.com>
2025-02-12 16:38:41 +0900

    향 테라피 연동

--- src/components/therapy/TherapyDiffuserRecommend.js
+++ src/components/therapy/TherapyDiffuserRecommend.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import styles from '../../css/therapy/TherapyDiffuserRecommend.module.css';
-import { useNavigate, useLocation } from 'react-router-dom';
-import diffuserData from '../../data/DiffuserData';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { 
+    selectRecommendations, 
+    selectUsageRoutine, 
+    selectImageUrls,
+    selectLoading,
+    selectTherapyTitle
+} from '../../module/TherapyModule';
+import ColorLoadingScreen from '../../components/loading/ColorLoadingScreen';
 
 const TherapyDiffuserRecommend = () => {
     const navigate = useNavigate();
-    // 이전 페이지에서 전달된 상태값을 받기 위한 location 훅
-    const location = useLocation();
-    // location.state에서 category와 keyword 추출 (값이 없을 경우 빈 객체 사용)
-    const { category } = location.state || {};
+    // Redux 상태 가져오기
+    const recommendations = useSelector(selectRecommendations);
+    const usageRoutine = useSelector(selectUsageRoutine);
+    const imageUrls = useSelector(selectImageUrls);
+    const loading = useSelector(selectLoading);
+    const therapyTitle = useSelector(selectTherapyTitle);
 
-    // 디버깅을 위한 로그
-    console.log('Location State:', location.state);
-    console.log('Category:', category);
-    console.log('Diffuser Data:', diffuserData);
-    console.log('Selected Data:', diffuserData[category]);
-
-    // 선택된 카테고리와 키워드에 해당하는 디퓨저 데이터 가져오기
-    const selectedData = diffuserData[category];
-
-    // 데이터가 없는 경우 에러 메시지 표시
-    if (!selectedData) {
-        return <div>데이터를 찾을 수 없습니다.</div>;
+    if (loading) {
+        return <ColorLoadingScreen loadingText="디퓨저를 찾는 중..." />;
     }
 
     return (
@@ -39,16 +39,16 @@ const TherapyDiffuserRecommend = () => {
             {/* 메인 컨텐츠 */}
             <h1 className={styles.mainTitle}>키워드에 적합한 디퓨저를 추천합니다.</h1>
             {/* 선택된 카테고리 태그 표시 */}
-            <p className={styles.categoryTag}>{selectedData.title}</p>
+            <p className={styles.categoryTag}>{therapyTitle}</p>
 
             {/* 디퓨저 추천 영역 */}
             <div className={styles.diffuserGrid}>
                 {/* 선택된 데이터의 제품들을 매핑하여 카드로 표시 */}
-                {selectedData.products.map((product, index) => (
+                {recommendations.map((product, index) => (
                     <div key={index} className={styles.diffuserCard}>
                         <div className={styles.cardFront}>
                             <img
-                                src={product.image}
+                                src={imageUrls[index]}
                                 alt={product.name}
                                 className={styles.diffuserImage}
                             />
@@ -66,7 +66,7 @@ const TherapyDiffuserRecommend = () => {
 
             {/* 루틴 설명 박스 */}
             <div className={styles.routineBox}>
-                <p className={styles.routineText}>{selectedData.routine}</p>
+                <p className={styles.routineText}>{usageRoutine}</p>
             </div>
         </div>
     );
